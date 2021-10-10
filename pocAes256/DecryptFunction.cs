4471824414d86773b11d4101de5a8725aa87aec5
Juan Camilo Colmenares Rocha <soadcam@gmail.com>
2021-10-09 20:43:36 -0500

    Upgrade dynamic IV key

--- pocAes256/DecryptFunction.cs
+++ pocAes256/DecryptFunction.cs
@@ -14,6 +14,8 @@ namespace pocAes256
 {
     public static class DecryptFunction
     {
+        private const string KEY = "v6K88GdfVAjgMEwV6OxRGKCA6E0sVE4T";
+
         [FunctionName("Decryption")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
@@ -27,32 +29,59 @@ namespace pocAes256
             string plainText = queryValue ?? Convert.ToString(data?.value);
 
             string responseMessage = DecryptRequest(plainText, log);
-            return new OkObjectResult(new { encrypted = responseMessage });
+            return new OkObjectResult(new { result = responseMessage });
         }
 
         private static string DecryptRequest(string dataToDecrypt, ILogger log)
         {
             try
             {
-                byte[] cryptkey = Encoding.UTF8.GetBytes("v6K88GdfVAjgMEwV6OxRGKCA6E0sVE4T");
-                byte[] initVector = Encoding.UTF8.GetBytes("WAbIiaaGqigzD4Zt");
-
-                using (var rijndaelManaged =
-                       new RijndaelManaged { Key = cryptkey, IV = initVector, Mode = CipherMode.CBC })
-                using (var memoryStream =
-                       new MemoryStream(Convert.FromBase64String(dataToDecrypt)))
-                using (var cryptoStream =
-                       new CryptoStream(memoryStream,
-                           rijndaelManaged.CreateDecryptor(cryptkey, initVector),
-                           CryptoStreamMode.Read))
-                {
-                    return new StreamReader(cryptoStream).ReadToEnd();
-                }
+                return AesDecrypt(dataToDecrypt, Encoding.UTF8.GetBytes(KEY));
             }
             catch (CryptographicException e)
             {
                 log.LogError("A Cryptographic error occurred: {0}", e.ToString());
                 return null;
+            }
+        }
+
+        private static string AesDecrypt(string data, byte[] key)
+        {
+            return Encoding.UTF8.GetString(AesDecrypt(Convert.FromBase64String(data), key));
+        }
+
+        private static byte[] AesDecrypt(byte[] data, byte[] key)
+        {
+            if (data == null || data.Length <= 0)
+            {
+                throw new ArgumentNullException($"{nameof(data)} cannot be empty");
+            }
+
+            using (var aes = new AesCryptoServiceProvider
+            {
+                Key = key,
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.PKCS7
+            })
+            {
+                var iv = new byte[16];
+                Array.Copy(data, 0, iv, 0, iv.Length);
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, aes.CreateDecryptor(aes.Key, iv), CryptoStreamMode.Write))
+                    using (var binaryWriter = new BinaryWriter(cs))
+                    {
+                        binaryWriter.Write(
+                            data,
+                            iv.Length,
+                            data.Length - iv.Length
+                        );
+                    }
+
+                    var dataBytes = ms.ToArray();
+
+                    return dataBytes;
+                }
             }
         }
     }
--- pocAes256/EncryptFunction.cs
+++ pocAes256/EncryptFunction.cs
@@ -14,6 +14,8 @@ namespace pocAes256
 {
     public static class EncryptFunction
     {
+        private const string KEY = "v6K88GdfVAjgMEwV6OxRGKCA6E0sVE4T";
+
         [FunctionName("Encryption")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
@@ -27,35 +29,56 @@ namespace pocAes256
             string plainText = queryValue ?? Convert.ToString(data?.value);
             
             string responseMessage = EncryptRequest(plainText, log);
-            return new OkObjectResult(new { encrypted = responseMessage });
+            return new OkObjectResult(new { result = responseMessage });
         }
 
         private static string EncryptRequest(string dataToEncrypt, ILogger log)
         {
             try
             {
-                byte[] cryptkey = Encoding.UTF8.GetBytes("v6K88GdfVAjgMEwV6OxRGKCA6E0sVE4T");
-                byte[] initVector = Encoding.UTF8.GetBytes("WAbIiaaGqigzD4Zt");
-
-                using (var rijndaelManaged =
-                       new RijndaelManaged { Key = cryptkey, IV = initVector, Mode = CipherMode.CBC })
-                using (var memoryStream = new MemoryStream())
-                using (var cryptoStream =
-                       new CryptoStream(memoryStream,
-                           rijndaelManaged.CreateEncryptor(cryptkey, initVector),
-                           CryptoStreamMode.Write))
-                {
-                    using (var ws = new StreamWriter(cryptoStream))
-                    {
-                        ws.Write(dataToEncrypt);
-                    }
-                    return Convert.ToBase64String(memoryStream.ToArray());
-                }
+                return AesEncrypt(dataToEncrypt, Encoding.UTF8.GetBytes(KEY));
             }
             catch (CryptographicException e)
             {
                 log.LogError("A Cryptographic error occurred: {0}", e.ToString());
                 return null;
+            }
+        }
+
+        private static string AesEncrypt(string data, byte[] key)
+        {
+            return Convert.ToBase64String(AesEncrypt(Encoding.UTF8.GetBytes(data), key));
+        }
+
+        private static byte[] AesEncrypt(byte[] data, byte[] key)
+        {
+            if (data == null || data.Length <= 0)
+            {
+                throw new ArgumentNullException($"{nameof(data)} cannot be empty");
+            }
+
+            using (var aes = new AesCryptoServiceProvider
+            {
+                Key = key,
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.PKCS7
+            })
+            {
+                aes.GenerateIV();
+                var iv = aes.IV;
+                using (var encrypter = aes.CreateEncryptor(aes.Key, iv))
+                using (var cipherStream = new MemoryStream())
+                {
+                    using (var tCryptoStream = new CryptoStream(cipherStream, encrypter, CryptoStreamMode.Write))
+                    using (var tBinaryWriter = new BinaryWriter(tCryptoStream))
+                    {
+                        cipherStream.Write(iv);
+                        tBinaryWriter.Write(data);
+                        tCryptoStream.FlushFinalBlock();
+                    }
+                    var cipherBytes = cipherStream.ToArray();
+                    return cipherBytes;
+                }
             }
         }
     }
