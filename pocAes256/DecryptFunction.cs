a3f6e9376769cb17a179c7c43c0614ed237735fc
Juan Camilo Colmenares Rocha <soadcam@gmail.com>
2021-10-21 20:43:41 -0500

    Upgrade decrypting

--- pocAes256/DecryptFunction.cs
+++ pocAes256/DecryptFunction.cs
@@ -15,7 +15,7 @@ namespace pocAes256
     public static class DecryptFunction
     {
         private const string KEY = "v6K88GdfVAjgMEwV6OxRGKCA6E0sVE4T";
-        
+
         private const int IV_SIZE = 16;
 
         [FunctionName("Decryption")]
@@ -31,30 +31,26 @@ namespace pocAes256
             string plainText = queryValue ?? Convert.ToString(data?.value);
 
             string responseMessage = DecryptRequest(plainText, log);
-            return new OkObjectResult(new { encrypted = responseMessage });
+            return new OkObjectResult(new { result = responseMessage });
         }
 
         private static string DecryptRequest(string dataToDecrypt, ILogger log)
         {
             try
             {
-                byte[] encryptedData = Convert.FromBase64String(dataToDecrypt);
-                var initVector = new byte[IV_SIZE];
-                Array.Copy(encryptedData, 0, initVector, 0, initVector.Length);
                 byte[] cryptkey = Encoding.UTF8.GetBytes(KEY);
-
-                // diff
-                int lengthData = encryptedData.Length - initVector.Length;
-                byte[] inputData = new byte[lengthData];
-                Array.Copy(encryptedData, initVector.Length, inputData, 0, inputData.Length);
+                string ivRaw = dataToDecrypt.Substring(0, IV_SIZE);
+                string inputRaw = dataToDecrypt.Substring(IV_SIZE);
+                byte[] ivData = Encoding.UTF8.GetBytes(ivRaw);
+                byte[] inputData = Convert.FromBase64String(inputRaw);
 
                 using (var rijndaelManaged =
-                       new RijndaelManaged { Mode = CipherMode.CBC, Padding = PaddingMode.PKCS7})
+                       new RijndaelManaged { Mode = CipherMode.CBC, Padding = PaddingMode.PKCS7 })
                 using (var memoryStream =
                        new MemoryStream(inputData))
                 using (var cryptoStream =
                        new CryptoStream(memoryStream,
-                           rijndaelManaged.CreateDecryptor(cryptkey, initVector),
+                           rijndaelManaged.CreateDecryptor(cryptkey, ivData),
                            CryptoStreamMode.Read))
                 {
                     return new StreamReader(cryptoStream).ReadToEnd();
