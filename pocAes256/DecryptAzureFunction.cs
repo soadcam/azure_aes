264d50852e8e802d3a9a5cccbd9a9de0f79949d9
soadcam <31525560+soadcam@users.noreply.github.com>
2021-11-05 01:17:17 -0500

    Update DecryptAzureFunction.cs

--- pocAes256/DecryptAzureFunction.cs
+++ pocAes256/DecryptAzureFunction.cs
@@ -7,13 +7,11 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 private const string KEY = "v6K88GdfVAjgMEwV6OxRGKCA6E0sVE4T";
-private const string APLHA = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 private const int IV_SIZE = 16;
 
 public static async Task<IActionResult> Run(HttpRequest req, ILogger log)
@@ -24,32 +22,31 @@ public static async Task<IActionResult> Run(HttpRequest req, ILogger log)
     string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
     dynamic data = JsonConvert.DeserializeObject(requestBody);
     string plainText = queryValue ?? Convert.ToString(data?.value);
-    string iv = GenerateIV();
 
-    string responseMessage = EncryptRequest(plainText, iv, log);
-    return new OkObjectResult(new { result = responseMessage, iv });
+    string responseMessage = DecryptRequest(plainText, log);
+    return new OkObjectResult(new { result = JsonConvert.DeserializeObject(responseMessage) });
 }
 
-private static string EncryptRequest(string dataToEncrypt, string iv, ILogger log)
+private static string DecryptRequest(string dataToDecrypt, ILogger log)
 {
     try
     {
         byte[] cryptkey = Encoding.UTF8.GetBytes(KEY);
-        byte[] initVector = Encoding.UTF8.GetBytes(iv);
+        string ivRaw = dataToDecrypt.Substring(0, IV_SIZE);
+        string inputRaw = dataToDecrypt.Substring(IV_SIZE);
+        byte[] ivData = Encoding.UTF8.GetBytes(ivRaw);
+        byte[] inputData = Convert.FromBase64String(inputRaw);
 
         using (var rijndaelManaged =
-                new RijndaelManaged { Key = cryptkey, IV = initVector, Mode = CipherMode.CBC, Padding = PaddingMode.PKCS7 })
-        using (var memoryStream = new MemoryStream())
+                new RijndaelManaged { Mode = CipherMode.CBC, Padding = PaddingMode.PKCS7 })
+        using (var memoryStream =
+                new MemoryStream(inputData))
         using (var cryptoStream =
                 new CryptoStream(memoryStream,
-                    rijndaelManaged.CreateEncryptor(cryptkey, initVector),
-                    CryptoStreamMode.Write))
+                    rijndaelManaged.CreateDecryptor(cryptkey, ivData),
+                    CryptoStreamMode.Read))
         {
-            using (var ws = new StreamWriter(cryptoStream))
-            {
-                ws.Write(dataToEncrypt);
-            }
-            return Convert.ToBase64String(memoryStream.ToArray());
+            return new StreamReader(cryptoStream).ReadToEnd();
         }
     }
     catch (CryptographicException e)
@@ -57,12 +54,4 @@ private static string EncryptRequest(string dataToEncrypt, string iv, ILogger lo
         log.LogError("A Cryptographic error occurred: {0}", e.ToString());
         return null;
     }
-}
-
-private static string GenerateIV()
-{
-    Random random = new Random();
-    string iv = new string(Enumerable.Repeat(APLHA, IV_SIZE)
-        .Select(s => s[random.Next(s.Length)]).ToArray());
-    return iv;
 }
